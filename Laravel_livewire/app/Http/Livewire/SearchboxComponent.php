b54fb7d470ae5bb26e22b13c39dccd76af20e877
Your Name <you@example.com>
2022-04-11 23:52:07 +0600

    create livewire component

---
+++ Laravel_livewire/app/Http/Livewire/SearchboxComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SearchboxComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.searchbox-component');
+    }
+}
